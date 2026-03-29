302e93cdfc75a104459078c81b98b1411cef0979
karvind08 <karvind08@gmail.com>
2026-03-29 17:27:56 +0530

    29 March 2026

--- App1/main.py
+++ App1/main.py
@@ -24,4 +24,26 @@ st.divider()
 chart_data = pd.DataFrame(np.random.randn(30,3),columns=["A","B","C"])
 st.line_chart(chart_data)
 st.divider()
-st.help(pd)
+# st.help(pd)
+# st.help(st)
+st.divider()
+st.html('<body><h1> Hello</></body>')
+st.divider()
+st.button('Click Me')
+st.divider()
+st.button("Reset", type="primary")
+st.divider()
+btn = st.button("Say hello")
+if btn:
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
+st.divider()
+left, middle, right = st.columns(3)
+if left.button("Plain button", width="stretch"):
+    left.markdown("You clicked the plain button.")
+if middle.button("Emoji button", icon="😃", width="stretch"):
+    middle.markdown("You clicked the emoji button.")
+if right.button("Material button", icon=":material/mood:", width="stretch"):
+    right.markdown("You clicked the Material button.")
+st.divider()
