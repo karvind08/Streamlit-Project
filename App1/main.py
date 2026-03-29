d8ec9418e54397b19cd7d8351d549d72d798265e
karvind08 <karvind08@gmail.com>
2026-03-29 16:47:51 +0530

    29 March 2026

--- App1/main.py
+++ App1/main.py
@@ -2,28 +2,21 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 st.title("My First Streamlit app")
-st.text("This is text\n[and more text](that's not a Markdown link).\n")
+st.divider()
+st.header('Header')
+st.divider()
+st.subheader('Subheader')
+st.divider()
+st.text("This is text\n It is in another line")
+st.divider()
 st.text("\nThis is another text",help='This is streamlit text')
-
+st.divider()
 code = '''def hello():
     print("Hello, Streamlit!")
     print('I am Arvind')'''
-st.code(code, language="Python",line_numbers=True,wrap_lines=True, height=80)
-code1 = '''Is it a crown or boat?
-                        ii
-                      iiiiii
-WWw                 .iiiiiiii.                ...:
- WWWWWWw          .iiiiiiiiiiii.         ........
-  WWWWWWWWWWw    iiiiiiiiiiiiiiii    ...........
-   WWWWWWWWWWWWWWwiiiiiiiiiiiiiiiii............
-    WWWWWWWWWWWWWWWWWWwiiiiiiiiiiiiii.........
-     WWWWWWWWWWWWWWWWWWWWWWwiiiiiiiiii.......
-      WWWWWWWWWWWWWWWWWWWWWWWWWWwiiiiiii....
-       WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWwiiii.
-          -MMMWWWWWWWWWWWWWWWWWWWWWWMMM-
-'''
-st.code(code1, language=None)
-
+st.code(code,language="Python",line_numbers=True,wrap_lines=True,height=None)
+# st.code(code, language="Python",line_numbers=True,wrap_lines=True, height=80)
+st.divider()
 # Line Chart
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
