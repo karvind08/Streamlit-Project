aba0148219bd4832f32e919c330c6d3cc4c1521e
karvind08 <karvind08@gmail.com>
2026-03-29 17:15:22 +0530

    29 March 2026

--- App1/main.py
+++ App1/main.py
@@ -11,13 +11,17 @@ st.text("This is text\n It is in another line")
 st.divider()
 st.text("\nThis is another text",help='This is streamlit text')
 st.divider()
+st.caption('This is caption')
+st.divider()
+st.markdown('*Hello* **This is another line** ***This is another***')
+st.divider()
 code = '''def hello():
     print("Hello, Streamlit!")
     print('I am Arvind')'''
 st.code(code,language="Python",line_numbers=True,wrap_lines=True,height=None)
-# st.code(code, language="Python",line_numbers=True,wrap_lines=True, height=80)
 st.divider()
 # Line Chart
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+chart_data = pd.DataFrame(np.random.randn(30,3),columns=["A","B","C"])
 st.line_chart(chart_data)
+st.divider()
+st.help(pd)
