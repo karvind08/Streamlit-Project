ee1e4085d0320046b62936fc96b07b56f41c0caf
karvind08 <karvind08@gmail.com>
2026-03-29 13:44:41 +0530

    29 March 2026

--- App1/main.py
+++ App1/main.py
@@ -1,38 +1,30 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+st.title("My First Streamlit app")
+st.text("This is text\n[and more text](that's not a Markdown link).\n")
+st.text("\nThis is another text",help='This is streamlit text')
 
-about_page = st.Page(
-    page = "views/about_me.py",
-    title = "About Us",
-    icon = ":material/account_circle:",
-    default = True,
-)
+code = '''def hello():
+    print("Hello, Streamlit!")
+    print('I am Arvind')'''
+st.code(code, language="python",line_numbers=True,wrap_lines=True, height=80)
+code1 = '''Is it a crown or boat?
+                        ii
+                      iiiiii
+WWw                 .iiiiiiii.                ...:
+ WWWWWWw          .iiiiiiiiiiii.         ........
+  WWWWWWWWWWw    iiiiiiiiiiiiiiii    ...........
+   WWWWWWWWWWWWWWwiiiiiiiiiiiiiiiii............
+    WWWWWWWWWWWWWWWWWWwiiiiiiiiiiiiii.........
+     WWWWWWWWWWWWWWWWWWWWWWwiiiiiiiiii.......
+      WWWWWWWWWWWWWWWWWWWWWWWWWWwiiiiiii....
+       WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWwiiii.
+          -MMMWWWWWWWWWWWWWWWWWWWWWWMMM-
+'''
+st.code(code1, language=None)
 
-project_1_page = st.Page(
-    page = "views/sales_dashboard.py",
-    title = "Sales Dashboard",
-    icon = ":material/bar_chart:",
-)
+# Line Chart
 
-project_2_page = st.Page(
-    page = "views/chatbot.py",
-    title = "Chat Bot",
-    icon = ":material/smart_toy:",
-)
-
-project_3_page = st.Page(
-    page = "views/contact.py",
-    title = "Contact",
-    icon = ":material/contact_page:",
-)
-#pg = st.navigation(pages=[about_page,project_1_page,project_2_page,project_3_page])
-
-pg = st.navigation(
-    {
-        "Info": [about_page,project_3_page],
-        "Projects": [project_1_page,project_2_page],
-    }
-)
-
-#st.logo("assets/logo.png")
-st.sidebar.text("Made with 💕 by Arvind")
-pg.run()
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.line_chart(chart_data)
