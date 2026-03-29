09265ee3a923b405af202b2d45e6b55771c380f1
karvind08 <karvind08@gmail.com>
2026-03-29 18:23:59 +0530

    29 March 2026

--- App2/home.py
+++ App2/home.py
@@ -1,1 +1,18 @@
 import streamlit as st
+about_page = st.Page(
+    page = 'pages/about.py',
+    title = 'About Me',
+    icon = ':material/home:',
+    default = True
+)
+
+contact_page = st.Page(
+    page='pages/contact.py',
+    title='Contact',
+    icon = ':material/home:'
+)
+
+pg = st.navigation(pages=[about_page,contact_page])
+st.logo("assets/heart.png")
+st.sidebar.text("Made with 💕 by Arvind")
+pg.run()
